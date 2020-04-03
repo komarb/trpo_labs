136fccecb8ab776dc9e402a165bfa0fca37a9005
Komar Bogdan <49122144+komarb@users.noreply.github.com>
2020-04-03 17:40:01 +0300

    Master #1
    * Added Timezone picker service with tests
    
    * Added docker deployment
    
    * Edited README.md
    
    Co-authored-by: Богдан Комар <adastras@MacBook-Pro-Bogdan.local>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# trpo_labs
+
+## React timezone selector
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm run test`
+
+Launches the test runner in the interactive watch mode.<br />
+
+
+### Deployment
+
+You can deploy this project for production with docker-compose:
+
+### `docker-compose -d up`
+
+When the container is running, service is available at [http://localhost:80](http://localhost:80)
---
+++ src/App.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import MyTimeZonePicker from './App'
+import App from './App'
+
+test('rendering time in Moscow', () => {
+    let d = new Date().toLocaleTimeString();
+    const { getByText } = render(<App/>);
+    expect(getByText("It is " + d +" in Moscow.")).toBeInTheDocument();
+});
+
+test('rendering timezonepicker with default value', () => {
+    const { getByText, getByTestId } = render(<MyTimeZonePicker />);
+    expect(getByText("(UTC-12:00) International Date Line West")).toBeInTheDocument();
+});
+
+jest.mock("react-select", () => ({ options, value, onChange }) => {
+    function handleChange(event) {
+        const option = options.find(
+            option => option.value === event.currentTarget.value
+        );
+        onChange(option);
+    }
+    return (
+        <select data-testid="select" value={value} onChange={handleChange}>
+            {options.map(({label, value}, index) => (
+                <option value={value} key={index}>
+                    {label}
+                </option>
+            ))}
+        </select>
+    );
+});
+
+test("work of a MyTimezonePicker", () => {
+    const { getByText, getByTestId } = render(<MyTimeZonePicker />);
+    expect(getByText("(UTC-12:00) International Date Line West")).toBeInTheDocument();
+    fireEvent.change(getByTestId("select"), { target: { value: "E. South America Standard Time" } });
+    expect(getByText("(UTC-03:00) Brasilia")).toBeInTheDocument();
+});
